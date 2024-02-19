6efb1e687ab4a15d4517cb08f88fa5513e823e87
Sofiermas <Volserma@yandex.ru>
2024-02-19 23:57:14 +0300

    add markup & background style

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,17 @@ module.exports = {
           'postcss-loader'
         ]
       },
+      {
+         test: /\.js$/,
+         use: 'babel-loader',
+         exclude: '/node_modules/'
+       },
+       // добавили правило для обработки файлов
+       {
+         // регулярное выражение, которое ищет все файлы с такими расширениями
+         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+         type: 'asset/resource'
+       },
     ]
   },
   plugins: [
