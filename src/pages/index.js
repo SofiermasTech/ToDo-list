f01cd15853bb57f9853c764996722f66e8a3cf1b
Sofiermas <Volserma@yandex.ru>
2024-02-21 00:05:56 +0300

    add fonts, btn & style for list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,26 @@
 import './index.css';
+
+const inputTitle = document.getElementById('input-title');
+const btnCreate = document.getElementById('btn-create');
+const listToDo = document.querySelector('.list-todo');
+const btnDeleted = document.querySelector('.list-todo__btn-deleted');
+
+btnCreate.addEventListener('click', () => {
+   if (inputTitle.value.length === 0) {
+      return;
+   }
+
+   listToDo.insertAdjacentHTML(
+      'beforeend',
+      getNoteTemplate(inputTitle.value)
+   )
+   inputTitle.value = '';
+})
+
+function getNoteTemplate(title) {
+   return `
+   <li class="list-todo__item">
+                     <p class="list-todo__item-title">${title}</p>
+                     <button class="list-todo__btn-deleted"></button>
+                  </li>`
+}
